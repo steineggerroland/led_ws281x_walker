9f3b6447522d8b356d82efcff2cc3dc8915e3053
Roland Steinegger <roland@steinegger.one>
2023-08-24 13:21:00 +0200

    Added sprinter monster

--- monster_types.py
+++ monster_types.py
@@ -1,13 +1,17 @@
 import colorsys
 import json
 import math
+from random import random, gauss
+from secrets import randbelow
+
 from rpi_ws281x import Color
 
 
 class Monster:
-    def __init__(self, speed=1, position=0):
+    def __init__(self, speed=1, position=0, description="a monster"):
         self.speed = speed
         self.position = position
+        self.description = description
 
     def glimmer(self):
         return {}
@@ -15,20 +19,20 @@ class Monster:
     def handleTick(self, tick):
         if (tick % self.speed) == 0:
             self.position += 1
+
     def json_sequence(self):
         encoder = json.JSONEncoder()
         sequence = [{"glimmer": {}, "positionChange": 1}]
         return encoder.encode(sequence)
 
 
-
 class DotMonster(Monster):
     def __init__(self, color, speed=10, glow=.5, glow_speed=200, position=0):
-        super().__init__(speed, position)
+        super().__init__(speed, position, f"dot monster: color {color_to_text(color)}, speed {speed}")
         self.glow = glow
         self.glow_speed = glow_speed
         self.base_color = color
-        self.base_hls = colorsys.rgb_to_hls(color[0]/255, color[1]/255, color[2]/255)
+        self.base_hls = colorsys.rgb_to_hls(color[0] / 255, color[1] / 255, color[2] / 255)
         self.color = Color(color[0], color[1], color[2], color[3])
         # calculate glimmer sequence
         self.glimmer_sequence = {}
@@ -53,7 +57,7 @@ class DotMonster(Monster):
     def handleTick(self, tick):
         variance = 2 * self.glow * divmod(tick, self.glow_speed)[
             1] / self.glow_speed
-        variance = 1 - variance if (self.glow) - variance > 0 else 1 - (
+        variance = 1 - variance if self.glow - variance > 0 else 1 - (
                 2 * self.glow - variance)
         rgb = colorsys.hls_to_rgb(self.base_hls[0],
                                   self.base_hls[1] * variance,
@@ -81,9 +85,13 @@ class DotMonster(Monster):
         return encoder.encode(sequence)
 
 
-class SneakingDotMonster(DotMonster):
+class SneakingDotMonster(Monster):
     def __init__(self, color, speed=10, glow=255, glow_speed=200, length=2):
-        super().__init__(color, speed, glow, glow_speed, -length - 1)
+        super().__init__(speed, -length - 1, f"sneaking monster: color {color_to_text(color)}, speed {speed}")
+        self.glow = glow
+        self.glow_speed = glow_speed
+        self.base_color = color
+        self.color = Color(color[0], color[1], color[2], color[3])
         self.length = length
         self.current_glimmer = {}
         for pos in range(self.length):
@@ -144,10 +152,15 @@ class SneakingDotMonster(DotMonster):
         return encoder.encode(sequence)
 
 
-class CaterpillarMonster(DotMonster):
+class CaterpillarMonster(Monster):
     def __init__(self, color, speed=20, length=2, step_length=6):
-        super().__init__(color, speed if speed % 2 == 1 else speed + 1, 0, 1,
-                         -length - 1)
+        super().__init__(speed if speed % 2 == 1 else speed + 1, -length - 1,
+                         f"caterpillar monster: color {color_to_text(color)}, speed {speed},"
+                         f" step length {step_length})")
+        self.glow = 0
+        self.glow_speed = 1
+        self.base_color = color
+        self.color = Color(color[0], color[1], color[2], color[3])
         self.step_length = step_length
         self.length = length
         self.current_glimmer = {}
@@ -214,3 +227,128 @@ class CaterpillarMonster(DotMonster):
             sequence.append({"glimmer": value})
         sequence[0]["positionChange"] = self.step_length
         return encoder.encode(sequence)
+
+
+class SprintMonster(Monster):
+    def __init__(self, color, sprint_distance=20, recovery_time=20, position=0):
+        self.sprint_distance = max(1, sprint_distance)
+        self.recovery_time = max(1, recovery_time)
+        super().__init__(self.sprint_distance + self.recovery_time,
+                         min(position % self.sprint_distance, self.sprint_distance),
+                         f"sprinter: color {color_to_text(color)}, sprinting distance {self.sprint_distance} "
+                         f"and recovery time {self.recovery_time}")
+        self.base_color = color
+        self.base_hls = colorsys.rgb_to_hls(color[0] / 255, color[1] / 255, color[2] / 255)
+        self.color = Color(color[0], color[1], color[2], color[3])
+        self.current_glimmer = self.color
+        # calculate glimmer sequence
+        self.glimmer_sequence = {}
+        # sprinters run each tick up to the sprint distance, thus add 1 for each tick
+        for tick in range(self.sprint_distance):
+            self.glimmer_sequence[tick] = {}
+            self.glimmer_sequence[tick][tick] = self.color
+        # sprinters are so fast, they create a (2 dot) trail while sprinting
+        for tick in range(self.sprint_distance):
+            if tick > 1:
+                dim = .6
+                rgb = colorsys.hls_to_rgb(self.base_hls[0],
+                                          dim * self.base_hls[1],
+                                          self.base_hls[2])
+                color = Color(round(rgb[0] * 255), round(rgb[1] * 255), round(rgb[2] * 255),
+                              max(0, min(255, round(dim * self.base_color[3]))))
+                self.glimmer_sequence[tick][tick - 1] = color
+            if tick > 2:
+                dim = .3
+                rgb = colorsys.hls_to_rgb(self.base_hls[0],
+                                          dim * self.base_hls[1],
+                                          self.base_hls[2])
+                color = Color(round(rgb[0] * 255), round(rgb[1] * 255), round(rgb[2] * 255),
+                              max(0, min(255, round(dim * self.base_color[3]))))
+                self.glimmer_sequence[tick][tick - 2] = color
+        # after running, sprinters have to recover energy for the next sprint
+        for tick in range(self.recovery_time):
+            dim = tick / self.recovery_time
+            rgb = colorsys.hls_to_rgb(self.base_hls[0],
+                                      dim * self.base_hls[1],
+                                      self.base_hls[2])
+            color = Color(round(rgb[0] * 255), round(rgb[1] * 255), round(rgb[2] * 255),
+                          max(0, min(255, round(dim * self.base_color[3]))))
+            self.glimmer_sequence[self.sprint_distance + tick] = {}
+            self.glimmer_sequence[self.sprint_distance + tick][self.sprint_distance] = color
+
+    def handleTick(self, tick):
+        self.current_glimmer = self.glimmer_sequence[tick % self.speed]
+        if (tick % self.speed) == 0:
+            self.position += self.speed
+
+    def glimmer(self):
+        return self.current_glimmer
+
+    def json_sequence(self):
+        encoder = json.JSONEncoder()
+        sequence = []
+        for key, value in self.glimmer_sequence.items():
+            sequence.append({"glimmer": value})
+            if key % self.speed == 0:
+                sequence[len(sequence) - 1]["positionChange"] = self.sprint_distance
+        return encoder.encode(sequence)
+
+
+def color_to_text(color):
+    h = colorsys.rgb_to_hls(color[0] / 255, color[1] / 255, color[2] / 255)[0]
+    if h < 10 / 360:
+        return "red"
+    elif h < 45 / 360:
+        return "orange"
+    elif h < 75 / 360:
+        return "yellow"
+    elif h < 150 / 360:
+        return "green"
+    elif h < 195 / 360:
+        return "cyan"
+    elif h < 255 / 360:
+        return "blue"
+    elif h < 285 / 360:
+        return "violet"
+    elif h < 315 / 360:
+        return "magenta"
+    elif h < 330 / 360:
+        return "pink"
+    else:
+        return "red"
+
+
+def random_monster(warm_colors, slow_movement, dimmed):
+    if warm_colors:
+        hue = (randbelow(3600) / 3601) * 60 / 360
+        hue = hue if hue < 30 / 360 else 1 - hue
+        saturation = 1
+    else:
+        hue = randbelow(3600) / 3601
+        saturation = 1
+
+    lightness = 0.05 if dimmed else max(min(gauss(0.4, 0.2), 0.5), 0.3)
+    c = colorsys.hls_to_rgb(hue, lightness, saturation)
+    color = [round(c[0] * 255), round(c[1] * 255), round(c[2] * 255),
+             round(lightness * 255) if saturation < 0.04 else 0]
+    dice = random()
+    if dice > 2 / 6:
+        speed = round(random() * 30) + 2
+        speed = speed * 10 if slow_movement else speed
+        monster_length = max(2, round(gauss(2, .8)))
+        return SneakingDotMonster(
+            color, speed, round(random() * .75 + .25), round(random() * 100) + 20, monster_length)
+    elif dice > 2 / 6:
+        speed = round(random() * 50) + 8
+        speed = speed * 10 if slow_movement else speed
+        monster_length = max(2, round(gauss(2, .8)))
+        step_length = max(3, round(gauss(5, 1.5)))
+        return CaterpillarMonster(color, speed, monster_length, step_length)
+    elif dice > 1 / 6:
+        monster_length = max(2, round(gauss(2, .8)))
+        return SprintMonster(color)
+    else:
+        speed = round(random() * 10) + 2
+        speed = speed * 10 if slow_movement else speed
+        return DotMonster(
+            color, speed, random() * .5 + .2, round(random() * 500) + 100)
--- random_monster_draw_runner.py
+++ random_monster_draw_runner.py
@@ -1,17 +1,12 @@
-import colorsys
-import uuid
-from os import mkdir, path
-from curses import wrapper
-import yaml
-import time
-from rpi_ws281x import ws, Color, Adafruit_NeoPixel
-from random import random, gauss
-from secrets import randbelow
 import argparse
-from led_strip_types import StripAdapter
-from monster_types import SneakingDotMonster, CaterpillarMonster, DotMonster
-from time import sleep, time
-from threading import Thread, Lock
+import uuid
+from curses import wrapper
+from os import mkdir, path
+from random import gauss
+from threading import Thread
+from time import sleep
+
+from monster_types import random_monster
 
 # Process arguments
 parser = argparse.ArgumentParser()
@@ -31,45 +26,6 @@ slow_movement = args.night_mode
 dimmed = args.night_mode or args.dimmed
 mean_seconds_to_spawn = args.mean_time_to_spawn if not args.night_mode or not args.mean_time_to_spawn == DEFAULT_TIME_TO_SPAWN else 45
 
-
-def random_monster(screen):
-    if warm_colors:
-        hue = (randbelow(3600) / 3601) * 60 / 360
-        hue = hue if hue < 30 / 360 else 1 - hue
-        saturation = 1
-    else:
-        hue = randbelow(3600) / 3601
-        saturation = 1
-
-    lightness = 0.05 if dimmed else max(min(gauss(0.4, 0.2), 0.5), 0.3)
-    c = colorsys.hls_to_rgb(hue, lightness, saturation)
-    color = [round(c[0] * 255), round(c[1] * 255), round(c[2] * 255),
-             round(lightness * 255) if saturation < 0.04 else 0]
-    dice = random()
-    if dice > 3 / 5:
-        speed = round(random() * 30) + 2
-        speed = speed * 10 if slow_movement else speed
-        monster_length = max(2, round(gauss(2, .8)))
-        screen.addstr(f"new sneaking monster ({color},{speed})")
-        return SneakingDotMonster(
-            color, speed, random() * .75 + .25,
-                          round(random() * 100) + 20, monster_length)
-    elif dice > 1 / 5:
-        speed = round(random() * 50) + 8
-        speed = speed * 10 if slow_movement else speed
-        monster_length = max(2, round(gauss(2, .8)))
-        step_length = max(3, round(gauss(5, 1.5)))
-        screen.addstr(f"new caterpillar monster ({color},{speed},{step_length})")
-        return CaterpillarMonster(color, speed, monster_length, step_length)
-    else:
-        speed = round(random() * 10) + 2
-        speed = speed * 10 if slow_movement else speed
-        screen.addstr(f"new dot monster ({color},{speed})")
-        return DotMonster(
-            color, speed, random() * .5 + .2,
-                          round(random() * 500) + 100)
-
-
 monster_dir = 'new_monsters'
 if not path.exists(monster_dir):
     mkdir(monster_dir)
@@ -86,7 +42,8 @@ def draw_new_monsters(screen):
             sleep(1)
         screen.move(1, 0)
         screen.insertln()
-        monster = random_monster(screen)
+        monster = random_monster(warm_colors, slow_movement, dimmed)
+        screen.addstr(monster.description)
         with open(f"{monster_dir}/monster-{uuid.uuid4()}.json", 'w') as f:
             f.write(monster.json_sequence())
         screen.refresh()
@@ -99,5 +56,6 @@ def main(screen):
     except KeyboardInterrupt:
         if monster_draw_thread.isAlive():
             monster_draw_thread.join()
+
 
 wrapper(main)
